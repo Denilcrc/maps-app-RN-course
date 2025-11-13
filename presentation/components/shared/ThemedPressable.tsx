bcdc0b48804bb2a34bf2f08ada89daba32b3a2a2
Denilson Cardenas <cardenasguillend@gmail.com>
2025-11-12 23:14:42 -0600

    sdk maps implemented

--- presentation/components/shared/ThemedPressable.tsx
+++ presentation/components/shared/ThemedPressable.tsx
@@ -1,26 +1,26 @@
-import React from 'react'
-import { Pressable, PressableProps, StyleSheet, Text } from 'react-native'
+import React from "react";
+import { Pressable, PressableProps, StyleSheet, Text } from "react-native";
 
 interface Props extends PressableProps {
-    children: string
+  children: string;
 }
 
 const ThemedPressable = ({ children, ...props }: Props) => {
   return (
     <Pressable style={styles.btnPrimary} {...props}>
-        <Text style={{ color: 'white' }}>{children}</Text>
+      <Text style={{ color: "white" }}>{children}</Text>
     </Pressable>
-  )
-}
+  );
+};
 
-export default ThemedPressable
+export default ThemedPressable;
 
 const styles = StyleSheet.create({
-    btnPrimary: {
-        backgroundColor: 'black',
-        paddingVertical: 10,
-        paddingHorizontal: 20,
-        borderRadius: 100,
-        margin: 10,
-    }
+  btnPrimary: {
+    backgroundColor: "black",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 100,
+    margin: 10,
+  },
 });
